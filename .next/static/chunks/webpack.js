e80d923bae45322fe5ae267e762d923790d02b47
Daniel Pape <danielwpape@gmail.com>
2022-01-12 11:27:36 +0000

    Scale fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6c933dc4247380952982"; }
+/******/ 		__webpack_require__.h = function() { return "8735957acb6fd481009d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
