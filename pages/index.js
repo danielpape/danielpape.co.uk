6809ce2e5f6d445e8fb7f9a87949edc678f20390
Daniel Pape <danielwpape@gmail.com>
2024-02-16 12:17:47 +0000

    Updated font: DM Sans

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,6 @@ import React from "react";
 
 import "@fontsource/dm-sans/400.css";
 import "@fontsource/dm-sans/500.css";
-import "@fontsource/space-mono";
-import "@fontsource/shippori-mincho";
 
 export default function Home() {
   return (
