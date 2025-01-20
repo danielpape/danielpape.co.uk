3c67ab4eabc3034bbb133c14b693ea29c13503f3
Daniel Pape <danielwpape@gmail.com>
2025-01-20 22:23:22 +0000

    Updated dark mode on experience page

--- components/experience.js
+++ components/experience.js
@@ -8,55 +8,81 @@ export default function Experience(props) {
       </div>
       <div className="md:col-start-6 md:col-span-7 col-start-1 col-span-12 flex flex-col gap-8 pb-16">
         <div class="grid grid-cols-1 gap-2 md:grid-cols-3">
-          <div class="font-medium">2018 - Present</div>
+          <div class="font-medium dark:text-slate-300 text-slate-700">
+            2018 - Present
+          </div>
           <div class="col-span-2 flex justify-between">
             <div>
-              <div class="font-bold">Next</div>
-              <div class="text-gray-600">Lead UX Designer</div>
+              <div class="font-bold dark:text-slate-200 text-slate-800">
+                Next
+              </div>
+              <div class="dark:text-slate-400 text-slate-600">
+                Lead UX Designer
+              </div>
             </div>
-            <div class="text-right">Hybrid - Leicester</div>
+            <div class="text-right dark:text-slate-300 text-slate-700">
+              Hybrid - Leicester
+            </div>
           </div>
         </div>
       </div>
       <div className="md:col-start-6 md:col-span-7 col-start-1 col-span-12 flex flex-col gap-8 pb-16">
         <div class="grid grid-cols-1 gap-2 md:grid-cols-3">
-          <div class="font-medium">2014 - 2018</div>
+          <div class="font-medium dark:text-slate-300 text-slate-700">
+            2014 - 2018
+          </div>
           <div class="col-span-2 flex justify-between">
             <div>
-              <div class="font-bold">
+              <div class="font-bold dark:text-slate-200 text-slate-800">
                 The Independent & London Evening Standard
               </div>
-              <div class="text-gray-600">Product Designer</div>
+              <div class="dark:text-slate-400 text-slate-600">
+                Product Designer
+              </div>
             </div>
-            <div class="text-right">London</div>
+            <div class="text-right dark:text-slate-300 text-slate-700">
+              London
+            </div>
           </div>
         </div>
       </div>
       <div className="md:col-start-6 md:col-span-7 col-start-1 col-span-12 flex flex-col gap-8 pb-16">
         <div class="grid grid-cols-1 gap-2 md:grid-cols-3">
-          <div class="font-medium">2014 - 2018</div>
+          <div class="font-medium dark:text-slate-300 text-slate-700">
+            2014 - 2018
+          </div>
           <div class="col-span-2 flex justify-between">
             <div>
-              <div class="font-bold">
+              <div class="font-bold dark:text-slate-200 text-slate-800">
                 The Independent & London Evening Standard
               </div>
-              <div class="text-gray-600">Product Designer</div>
+              <div class="dark:text-slate-400 text-slate-600">
+                Product Designer
+              </div>
             </div>
-            <div class="text-right">London</div>
+            <div class="text-right dark:text-slate-300 text-slate-700">
+              London
+            </div>
           </div>
         </div>
       </div>
       <div className="md:col-start-6 md:col-span-7 col-start-1 col-span-12 flex flex-col gap-8 pb-16">
         <div class="grid grid-cols-1 gap-2 md:grid-cols-3">
-          <div class="font-medium">2014 - 2018</div>
+          <div class="font-medium dark:text-slate-300 text-slate-700">
+            2014 - 2018
+          </div>
           <div class="col-span-2 flex justify-between">
             <div>
-              <div class="font-bold">
+              <div class="dark:text-slate-400 text-slate-600">
                 The Independent & London Evening Standard
               </div>
-              <div class="text-gray-600">Product Designer</div>
+              <div class="dark:text-slate-400 text-slate-600">
+                Product Designer
+              </div>
             </div>
-            <div class="text-right">London</div>
+            <div class="text-right dark:text-slate-300 text-slate-700">
+              London
+            </div>
           </div>
         </div>
       </div>
