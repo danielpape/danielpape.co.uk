7b384a2eb6f3624f9d5c33c4c8dead7d0bc51dbb
Daniel Pape <danielwpape@gmail.com>
2026-01-05 11:03:51 +0000

    Blog Fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 module.exports = {
   reactStrictMode: true,
+
+  // Required for GitHub Pages static export
+  output: "export",
+  trailingSlash: true,
+
   images: {
     loader: "custom",
   },
+
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
