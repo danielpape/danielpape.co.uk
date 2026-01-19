827f4d5d75952136ec9e3e935cb99fc8582a17d0
Daniel Pape <danielwpape@gmail.com>
2026-01-19 10:57:11 +0000

    Updated Bento CSS

--- components/sections/Grid.js
+++ components/sections/Grid.js
@@ -9,44 +9,44 @@ import WarehouseIcon from "../icons/WarehouseIcon";
 
 export default function Grid(props) {
   return (
-    <div
-      className="grid grid-cols-1 md:grid-cols-4 md:grid-rows-2 gap-4 md:h-[312px] px-4 my-12 max-w-screen-xl m-auto
-    "
-    >
-      <div className="md:col-start-2 md:col-end-4 md:row-start-1 md:row-end-3 md:max-h-[500px] max-h-[240px] border border-slate-300 rounded-md overflow-hidden">
+    <div class="bento">
+      <div class="bento__item bento__item--large">
         <img
           src="/images/medori.png"
-          className="w-full h-full object-cover"
+          class="bento__media"
           alt="A person's hands holding a phone displaying the Medori app which lists medications and which have been taken"
         />
       </div>
-      <div className="md:col-start-4 md:col-end-6 md:row-start-1 md:row-end-3 border border-slate-300 rounded-md overflow-hidden">
+
+      <div class="bento__item bento__item--tall">
         <img
           src="/images/nextpay.png"
-          className="w-full h-full object-cover"
+          class="bento__media"
           alt="A person's hands holding a phone displaying the Next Pay credit overlay"
         />
       </div>
-      <div className="flex items-center justify-center md:col-start-1 md:col-end-2 md:row-start-1 md:row-end-2 min-h-[140px] max-h-[240px] border border-slate-300 rounded-md overflow-hidden max-h-[160px] bg-white">
+
+      <div class="bento__item bento__item--video">
         <video
           src="/videos/TagAnimation.mp4"
-          className="max-w-[240px] p-8"
-          autoPlay
+          class="bento__video"
+          autoplay
           loop
           muted
-          playsInline
+          playsinline
         >
           Your browser does not support the video tag.
         </video>
       </div>
-      <div className="md:col-start-1 md:col-end-2 md:row-start-2 md:row-end-3 flex flex-col items-center p-2 justify-center gap-1 bg-slate-50 border border-slate-300 rounded-md overflow-hidden min-h-[140px]">
-        <div className="flex flex-row gap-4">
+
+      <div class="bento__item bento__item--icons">
+        <div class="bento__icon-row">
           <HomeIcon />
           <StoreIcon />
           <WarehouseIcon />
           <LorryIcon />
         </div>
-        <div className="flex flex-row gap-4">
+        <div class="bento__icon-row">
           <DesktopIcon />
           <FactoryIcon />
           <ParcelIcon />
